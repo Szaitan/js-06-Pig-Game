05876014fbadf1e879dab34a96df0ec2eacfd3cf
Michal <m.krepiniewicz@gmail.com>
2024-11-09 16:04:07 +0100

    testing this in diffrent situation

--- test.js
+++ test.js
@@ -1,9 +1,19 @@
-test1();
-function test1() {
-  console.log('test1');
-}
+'use strict';
 
-test2();
-var test2 = function () {
-  console.log('test2');
+const mike = {
+  name: 'Michał',
+  birthYear: 1990,
+  calcAge: function () {
+    return 2024 - this.birthYear;
+  },
 };
+
+console.log(mike.calcAge());
+
+const matilda = {
+  birthYear: 2017,
+};
+
+matilda.calcAge = mike.calcAge;
+
+console.log(matilda.calcAge());
